48e0393e3ca1c8422972f7b7feea45305aae17d7
fellipe52 <46055176+fellipe52@users.noreply.github.com>
2025-06-09 21:41:35 -0300

    criei nova secret

--- src/Pagamento.API/Program.cs
+++ src/Pagamento.API/Program.cs
@@ -19,10 +19,11 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
-
+//if (app.Environment.IsDevelopment())
+//{
 app.UseSwagger();
 app.UseSwaggerUI();
-
+//}
 
 app.UseAuthentication();
 app.UseAuthorization();
