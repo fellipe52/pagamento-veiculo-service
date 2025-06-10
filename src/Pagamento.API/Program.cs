34be109c91a0b28277b7057365563372d35a5970
fellipe52 <46055176+fellipe52@users.noreply.github.com>
2025-06-09 21:24:04 -0300

    utilização swagger

--- src/Pagamento.API/Program.cs
+++ src/Pagamento.API/Program.cs
@@ -19,11 +19,10 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthentication();
 app.UseAuthorization();
